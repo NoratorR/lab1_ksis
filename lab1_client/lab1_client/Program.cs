be934f51c29f8d46dbe6b522c33805fc136eaa08
Matvej Kachanka <matvej97@mail.ru>
2017-03-03 13:16:07 +0300

    lab1 final edition

---
+++ lab1_client/lab1_client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace lab1_client
+{
+    class Program
+    {
+        private const int listenPort = 11000;
+
+        public static void StartListen()
+        {
+            bool done = false;
+            Socket listner = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 11000);
+            listner.Bind(groupEP);
+            var EP = groupEP as EndPoint;
+            try
+            {
+                while (!done)
+                {
+                    Console.WriteLine("I'm client.");
+                    byte[] bytes = new byte[1024];
+                    listner.ReceiveFrom(bytes, ref EP);
+                    Console.Clear();
+                    Console.WriteLine("Time on server {0} ", Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+
+            }
+            finally
+            {
+                listner.Close();
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            StartListen();
+
+        }
+    }
+}
---
+++ lab1_server/lab1_server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket socet = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        socet.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+        
+        IPEndPoint endP = new IPEndPoint(IPAddress.Broadcast, 11000);
+        socet.Connect(endP);
+
+        Console.WriteLine("Server Work!");
+        while (true)
+        {
+        System.Threading.Thread.Sleep(1000);
+        string curTime = DateTime.Now.ToLongTimeString();
+        byte[] sendbuf = Encoding.ASCII.GetBytes(curTime);
+        socet.Send(sendbuf); 
+        }
+    }
+}
